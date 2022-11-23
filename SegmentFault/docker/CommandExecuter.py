e048c982d80aac750c3909778127de5598b444d0
markbozsoki <83731022+markbozsoki@users.noreply.github.com>
2022-11-23 13:23:12 +0100

    ADD: CommandExecuter.py - script running the predefined commands in commands.txt

---
+++ SegmentFault/docker/CommandExecuter.py
@@ -0,0 +1,62 @@
+"""
+*********************************************************************
+*********************************************************************
+PROJECT_NAME : PacMan
+FILENAME     : CommandExecuter.py
+AUTHOR       : Bozsóki Márk
+UNIVERSITY   : BME
+TEAM         : SegmentFault
+*********************************************************************
+*********************************************************************
+Short description
+--------------------------------------------------------------------
+Script responsible for reading predefined commands from file
+and running them
+
+********************************************************************
+********************************************************************
+"""
+
+import os
+
+def GetCommads(sourceFile: str) -> list[str]:
+    """
+    Function reading the preaperd commands from file
+
+    @args:
+        sourceFile [str] - The path to the file containing the commannds
+    @return:
+        commands [list(str)] - The list of runnable commands
+    """
+    source = open(sourceFile, 'r')
+    result = []
+    while True:
+            line = source.readline()
+
+            # EndOfFile
+            if not line:
+                break
+
+            if list(line)[-1] == '\n':
+                result.append(list(line)[:-1])
+            else:
+                result.append(list(line))
+
+    source.close()
+    return result
+
+
+def RunCommands(commands: list[str]) -> None:
+    """
+    Function running the required CLI commands
+
+    @args:
+        commands [list(str)] - The list of runnable commands
+    """
+    for cmd in commands:
+        os.system(cmd)
+
+
+if __name__ == "__main__":
+    commands = GetCommads("commands.txt")
+    RunCommands(commands)
